00e02f6d79ef2c11d6c1f31647909076c3e641c2
Artem <aaddeewwqqaa@gmail.com>
2018-02-18 21:40:41 +0300

    Template of ExcelWriter.

--- Helpers/ExcelWriter.cs
+++ Helpers/ExcelWriter.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using OfficeOpenXml;
 
 namespace EMS.Desktop.Helpers
 {
     class ExcelWriter
     {
+        private static void main()
+        {
+            using (var excel = new ExcelPackage())
+            {
+                var ws = excel.Workbook.Worksheets.Add("WorkSheetName");
 
+                string[] str = File.ReadAllLines("Путь к файлу.210");
+                for (int i = 0; i < str.Length; i++)
+                    for (int j = 0; j < str[i].Split('^').Length; j++)
+                        ws.Cells[i + 1, j + 1].Value = str[i].Split('^')[j];
+
+                excel.SaveAs(new FileInfo("Путь к файлу.xlsx"));
+            }
+        }
     }
 }
