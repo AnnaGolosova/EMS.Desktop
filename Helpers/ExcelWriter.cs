43b4cabf620b5b379a7e03e85838af6edb903fc4
Artem <aaddeewwqqaa@gmail.com>
2018-02-21 22:16:52 +0300

    Almost workable ExcelWriter. (Fixed errors of previous commit.)

--- Helpers/ExcelWriter.cs
+++ Helpers/ExcelWriter.cs
@@ -9,26 +9,25 @@ namespace EMS.Desktop.Helpers
     {
         void Read210(Models.Report210 datas, string firstline)
         {
-            List<string> file210 = File.ReadAllLines(new ConfigAppManager().GetReports210Path());
+            string[] file210 = File.ReadAllLines("D:\\test.210"/*new ConfigAppManager().GetReports210Path()*/);
             firstline = file210[0];
-            file210.RemoveAt(0);
-            //foreach (string s in file210)
-            for (int i = 0; i < file210.Count; i++)
+            datas.Datas = new List<Models.Report210.ReportData>();
+            for (int i = 1; i < file210.Length; i++)
             {
                 datas.Datas.Add(new Models.Report210.ReportData());
-                datas.Datas[i].Id = Convert.ToInt32(file210[i].Split('^')[0]);
-                datas.Datas[i].RateId = Convert.ToInt32(file210[i].Split('^')[1]);
-                datas.Datas[i].HomeSteadNumber = Convert.ToInt32(file210[i].Split('^')[2]);
-                datas.Datas[i].OwnerName = file210[i].Split('^')[3];
-                datas.Datas[i].Date = Convert.ToDateTime(file210[i].Split('^')[4]);
-                datas.Datas[i].Introduced = Convert.ToDouble(file210[i].Split('^')[5]);
-                datas.Datas[i].Arrer = Convert.ToDouble(file210[i].Split('^')[6]);
-                datas.Datas[i].Entered = Convert.ToDouble(file210[i].Split('^')[7]);
-                datas.Datas[i].Code = file210[i].Split('^')[8] + "^" + file210[i].Split('^')[9] + "^" + file210[i].Split('^')[10] + "^" + file210[i].Split('^')[11] + "^" + file210[i].Split('^')[12] + "^" + file210[i].Split('^')[13] + "^" + file210[i].Split('^')[14] + "^" + file210[i].Split('^')[15] + "^" + file210[i].Split('^')[16] + "^" + file210[i].Split('^')[17] + "^" + file210[i].Split('^')[18] + "^" + file210[i].Split('^')[19];
+                datas.Datas[i - 1].Id = Convert.ToInt32(file210[i].Split('^')[0]);
+                datas.Datas[i - 1].RateId = Convert.ToInt32(file210[i].Split('^')[1]);
+                datas.Datas[i - 1].HomeSteadNumber = Convert.ToInt32(file210[i].Split('^')[2]);
+                datas.Datas[i - 1].OwnerName = file210[i].Split('^')[3];
+                datas.Datas[i - 1].Date = new DateTime(Convert.ToInt32(file210[i].Split('^')[5].Split('.')[1]), Convert.ToInt32(file210[i].Split('^')[5].Split('.')[0]), 1);
+                datas.Datas[i - 1].Introduced = Convert.ToDouble(file210[i].Split('^')[6].Replace(".", ","));
+                datas.Datas[i - 1].Arrer = Convert.ToDouble(file210[i].Split('^')[7].Replace(".", ","));
+                datas.Datas[i - 1].Entered = Convert.ToDouble(file210[i].Split('^')[8].Replace(".", ","));
+                datas.Datas[i - 1].Code = file210[i].Split('^')[9] + "^" + file210[i].Split('^')[10] + "^" + file210[i].Split('^')[11] + "^" + file210[i].Split('^')[12] + "^" + file210[i].Split('^')[13] + "^" + file210[i].Split('^')[14] + "^" + file210[i].Split('^')[15] + "^" + file210[i].Split('^')[16] + "^" + file210[i].Split('^')[17] + "^" + file210[i].Split('^')[18] + "^" + file210[i].Split('^')[19];
             }
         }
 
-        private static void main()
+        private static void Main()
         {
             Models.Report210 datas = new Models.Report210();
             string firstline = "";
@@ -42,12 +41,12 @@ namespace EMS.Desktop.Helpers
                     ws.Cells[x.Id, 2].Value = x.RateId;
                     ws.Cells[x.Id, 3].Value = x.HomeSteadNumber;
                     ws.Cells[x.Id, 4].Value = x.OwnerName;
-                    ws.Cells[x.Id, 5].Value = x.Date;
+                    ws.Cells[x.Id, 5].Value = Convert.ToString(x.Date);
                     ws.Cells[x.Id, 6].Value = x.Introduced;
                     ws.Cells[x.Id, 7].Value = x.Arrer;
                     ws.Cells[x.Id, 8].Value = x.Entered;
                 }
-                excel.SaveAs(new FileInfo(new ConfigAppManager().GetExcelPath()));
+                excel.SaveAs(new FileInfo("D:\\test.xlsx"/*new ConfigAppManager().GetExcelPath()*/));
             }
         }
     }
