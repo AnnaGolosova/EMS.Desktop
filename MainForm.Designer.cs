6f082f8b774b4ffeb00d9dab7eaf0c820023ff29
advden4ik <danila120900@mail.ru>
2018-04-04 08:29:12 +0300

    resolved task load files

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -85,8 +85,7 @@
             this.MenuAboutProgram});
             this.Menu.Location = new System.Drawing.Point(0, 0);
             this.Menu.Name = "Menu";
-            this.Menu.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.Menu.Size = new System.Drawing.Size(1019, 28);
+            this.Menu.Size = new System.Drawing.Size(764, 24);
             this.Menu.TabIndex = 0;
             this.Menu.Text = "menuStrip1";
             // 
@@ -96,13 +95,13 @@
             this.ToolStripDownloadNewFile,
             this.CLearFileStatesMI});
             this.MenuFail.Name = "MenuFail";
-            this.MenuFail.Size = new System.Drawing.Size(57, 24);
+            this.MenuFail.Size = new System.Drawing.Size(48, 20);
             this.MenuFail.Text = "Файл";
             // 
             // ToolStripDownloadNewFile
             // 
             this.ToolStripDownloadNewFile.Name = "ToolStripDownloadNewFile";
-            this.ToolStripDownloadNewFile.Size = new System.Drawing.Size(282, 26);
+            this.ToolStripDownloadNewFile.Size = new System.Drawing.Size(232, 22);
             this.ToolStripDownloadNewFile.Text = "Загрузка новых файлов";
             this.ToolStripDownloadNewFile.Click += new System.EventHandler(this.ToolStripDownloadNewFile_Click);
             // 
@@ -110,7 +109,7 @@
             // 
             this.CLearFileStatesMI.Enabled = false;
             this.CLearFileStatesMI.Name = "CLearFileStatesMI";
-            this.CLearFileStatesMI.Size = new System.Drawing.Size(282, 26);
+            this.CLearFileStatesMI.Size = new System.Drawing.Size(232, 22);
             this.CLearFileStatesMI.Text = "Сбросить состояния файлов";
             this.CLearFileStatesMI.Click += new System.EventHandler(this.CLearFileStatesMI_Click);
             // 
@@ -120,58 +119,57 @@
             this.MenuReposForERIP,
             this.MenuReposForExcel});
             this.MenuRepos.Name = "MenuRepos";
-            this.MenuRepos.Size = new System.Drawing.Size(71, 24);
+            this.MenuRepos.Size = new System.Drawing.Size(60, 20);
             this.MenuRepos.Text = "Отчёты";
             // 
             // MenuReposForERIP
             // 
             this.MenuReposForERIP.Name = "MenuReposForERIP";
-            this.MenuReposForERIP.Size = new System.Drawing.Size(181, 26);
+            this.MenuReposForERIP.Size = new System.Drawing.Size(135, 22);
             this.MenuReposForERIP.Text = "Отчёт .202";
             this.MenuReposForERIP.Click += new System.EventHandler(this.MenuReposForERIP_Click);
             // 
             // MenuReposForExcel
             // 
             this.MenuReposForExcel.Name = "MenuReposForExcel";
-            this.MenuReposForExcel.Size = new System.Drawing.Size(181, 26);
+            this.MenuReposForExcel.Size = new System.Drawing.Size(135, 22);
             this.MenuReposForExcel.Text = "Отчёт Excel";
             this.MenuReposForExcel.Click += new System.EventHandler(this.MenuReposForExcel_Click);
             // 
             // MenuDocuments
             // 
             this.MenuDocuments.Name = "MenuDocuments";
-            this.MenuDocuments.Size = new System.Drawing.Size(148, 24);
+            this.MenuDocuments.Size = new System.Drawing.Size(119, 20);
             this.MenuDocuments.Text = "Просмотр данных";
             this.MenuDocuments.Click += new System.EventHandler(this.MenuDocuments_Click);
             // 
             // MainJournal
             // 
             this.MainJournal.Name = "MainJournal";
-            this.MainJournal.Size = new System.Drawing.Size(75, 24);
+            this.MainJournal.Size = new System.Drawing.Size(63, 20);
             this.MainJournal.Text = "Журнал";
             this.MainJournal.Click += new System.EventHandler(this.MainJournal_Click);
             // 
             // MenuSettings
             // 
             this.MenuSettings.Name = "MenuSettings";
-            this.MenuSettings.Size = new System.Drawing.Size(96, 24);
+            this.MenuSettings.Size = new System.Drawing.Size(79, 20);
             this.MenuSettings.Text = "Настройки";
             this.MenuSettings.Click += new System.EventHandler(this.MenuSettings_Click);
             // 
             // MenuAboutProgram
             // 
             this.MenuAboutProgram.Name = "MenuAboutProgram";
-            this.MenuAboutProgram.Size = new System.Drawing.Size(116, 24);
+            this.MenuAboutProgram.Size = new System.Drawing.Size(94, 20);
             this.MenuAboutProgram.Text = "О программе";
             this.MenuAboutProgram.Click += new System.EventHandler(this.MenuAboutProgram_Click);
             // 
             // MainProgressBar
             // 
             this.MainProgressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.MainProgressBar.Location = new System.Drawing.Point(0, 511);
-            this.MainProgressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.MainProgressBar.Location = new System.Drawing.Point(0, 415);
             this.MainProgressBar.Name = "MainProgressBar";
-            this.MainProgressBar.Size = new System.Drawing.Size(1019, 28);
+            this.MainProgressBar.Size = new System.Drawing.Size(764, 23);
             this.MainProgressBar.TabIndex = 1;
             // 
             // ColorPanel
@@ -181,10 +179,9 @@
             this.ColorPanel.Controls.Add(this.LoadingLabel);
             this.ColorPanel.Controls.Add(this.dataGridView1);
             this.ColorPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ColorPanel.Location = new System.Drawing.Point(0, 28);
-            this.ColorPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.ColorPanel.Location = new System.Drawing.Point(0, 24);
             this.ColorPanel.Name = "ColorPanel";
-            this.ColorPanel.Size = new System.Drawing.Size(1019, 483);
+            this.ColorPanel.Size = new System.Drawing.Size(764, 391);
             this.ColorPanel.TabIndex = 2;
             // 
             // ArrearGB
@@ -193,8 +190,10 @@
             this.ArrearGB.Controls.Add(this.ArrearEditDGV);
             this.ArrearGB.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ArrearGB.Location = new System.Drawing.Point(0, 0);
+            this.ArrearGB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ArrearGB.Name = "ArrearGB";
-            this.ArrearGB.Size = new System.Drawing.Size(1019, 483);
+            this.ArrearGB.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ArrearGB.Size = new System.Drawing.Size(764, 391);
             this.ArrearGB.TabIndex = 3;
             this.ArrearGB.TabStop = false;
             this.ArrearGB.Text = "Отредактируйте задолженности";
@@ -203,9 +202,10 @@
             // ArrearConfirmB
             // 
             this.ArrearConfirmB.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ArrearConfirmB.Location = new System.Drawing.Point(3, 447);
+            this.ArrearConfirmB.Location = new System.Drawing.Point(2, 362);
+            this.ArrearConfirmB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ArrearConfirmB.Name = "ArrearConfirmB";
-            this.ArrearConfirmB.Size = new System.Drawing.Size(1013, 33);
+            this.ArrearConfirmB.Size = new System.Drawing.Size(760, 27);
             this.ArrearConfirmB.TabIndex = 3;
             this.ArrearConfirmB.Text = "Сохранить";
             this.ArrearConfirmB.UseVisualStyleBackColor = true;
@@ -235,12 +235,13 @@
             dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.ArrearEditDGV.DefaultCellStyle = dataGridViewCellStyle8;
             this.ArrearEditDGV.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ArrearEditDGV.Location = new System.Drawing.Point(3, 18);
+            this.ArrearEditDGV.Location = new System.Drawing.Point(2, 15);
+            this.ArrearEditDGV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ArrearEditDGV.Name = "ArrearEditDGV";
             this.ArrearEditDGV.RowHeadersVisible = false;
             this.ArrearEditDGV.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;
             this.ArrearEditDGV.RowTemplate.Height = 24;
-            this.ArrearEditDGV.Size = new System.Drawing.Size(1013, 462);
+            this.ArrearEditDGV.Size = new System.Drawing.Size(760, 374);
             this.ArrearEditDGV.TabIndex = 2;
             this.ArrearEditDGV.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.ArrearEditDGV_CellValidating);
             // 
@@ -258,7 +259,7 @@
             this.ServiceId.HeaderText = "Услуга";
             this.ServiceId.Name = "ServiceId";
             this.ServiceId.ReadOnly = true;
-            this.ServiceId.Width = 81;
+            this.ServiceId.Width = 68;
             // 
             // HomesteadNumber
             // 
@@ -268,7 +269,7 @@
             this.HomesteadNumber.HeaderText = "Номер участка";
             this.HomesteadNumber.Name = "HomesteadNumber";
             this.HomesteadNumber.ReadOnly = true;
-            this.HomesteadNumber.Width = 125;
+            this.HomesteadNumber.Width = 99;
             // 
             // OwnerName
             // 
@@ -279,7 +280,7 @@
             this.OwnerName.Name = "OwnerName";
             this.OwnerName.ReadOnly = true;
             this.OwnerName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.OwnerName.Width = 79;
+            this.OwnerName.Width = 62;
             // 
             // Introduced
             // 
@@ -289,7 +290,7 @@
             this.Introduced.HeaderText = "Внесено";
             this.Introduced.Name = "Introduced";
             this.Introduced.ReadOnly = true;
-            this.Introduced.Width = 93;
+            this.Introduced.Width = 75;
             // 
             // Arrear
             // 
@@ -307,7 +308,7 @@
             this.Difference.HeaderText = "Количество кВ/ч";
             this.Difference.Name = "Difference";
             this.Difference.ReadOnly = true;
-            this.Difference.Width = 135;
+            this.Difference.Width = 107;
             // 
             // Value
             // 
@@ -317,7 +318,7 @@
             this.Value.HeaderText = "Последнее значение счетчика";
             this.Value.Name = "Value";
             this.Value.ReadOnly = true;
-            this.Value.Width = 151;
+            this.Value.Width = 116;
             // 
             // LoadingLabel
             // 
@@ -327,10 +328,9 @@
             this.LoadingLabel.AutoSize = true;
             this.LoadingLabel.BackColor = System.Drawing.Color.Transparent;
             this.LoadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.LoadingLabel.Location = new System.Drawing.Point(463, 218);
-            this.LoadingLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LoadingLabel.Location = new System.Drawing.Point(347, 177);
             this.LoadingLabel.Name = "LoadingLabel";
-            this.LoadingLabel.Size = new System.Drawing.Size(106, 24);
+            this.LoadingLabel.Size = new System.Drawing.Size(83, 18);
             this.LoadingLabel.TabIndex = 1;
             this.LoadingLabel.Text = "Загрузка...";
             this.LoadingLabel.Visible = false;
@@ -341,10 +341,10 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(1019, 483);
+            this.dataGridView1.Size = new System.Drawing.Size(764, 391);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.Visible = false;
             // 
@@ -356,10 +356,9 @@
             this.LabelProgrBar.Cursor = System.Windows.Forms.Cursors.Default;
             this.LabelProgrBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.LabelProgrBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.LabelProgrBar.Location = new System.Drawing.Point(0, 511);
-            this.LabelProgrBar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelProgrBar.Location = new System.Drawing.Point(0, 415);
             this.LabelProgrBar.Name = "LabelProgrBar";
-            this.LabelProgrBar.Size = new System.Drawing.Size(1019, 28);
+            this.LabelProgrBar.Size = new System.Drawing.Size(764, 23);
             this.LabelProgrBar.TabIndex = 0;
             this.LabelProgrBar.Text = "Файлы загружены";
             this.LabelProgrBar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -370,18 +369,20 @@
             this.AmountLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.AmountLabel.AutoSize = true;
             this.AmountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.AmountLabel.Location = new System.Drawing.Point(786, 7);
+            this.AmountLabel.Location = new System.Drawing.Point(590, 6);
+            this.AmountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.AmountLabel.Name = "AmountLabel";
-            this.AmountLabel.Size = new System.Drawing.Size(183, 17);
+            this.AmountLabel.Size = new System.Drawing.Size(150, 13);
             this.AmountLabel.TabIndex = 3;
             this.AmountLabel.Text = "Сумма на р/с : 100 BYN";
             // 
             // createExcelButton
             // 
             this.createExcelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.createExcelButton.Location = new System.Drawing.Point(850, 511);
+            this.createExcelButton.Location = new System.Drawing.Point(638, 415);
+            this.createExcelButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.createExcelButton.Name = "createExcelButton";
-            this.createExcelButton.Size = new System.Drawing.Size(169, 28);
+            this.createExcelButton.Size = new System.Drawing.Size(127, 23);
             this.createExcelButton.TabIndex = 4;
             this.createExcelButton.Text = "Сохранить в Excel";
             this.createExcelButton.UseVisualStyleBackColor = true;
@@ -390,9 +391,9 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1019, 539);
+            this.ClientSize = new System.Drawing.Size(764, 438);
             this.Controls.Add(this.createExcelButton);
             this.Controls.Add(this.AmountLabel);
             this.Controls.Add(this.LabelProgrBar);
@@ -401,8 +402,7 @@
             this.Controls.Add(this.Menu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.Menu;
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimumSize = new System.Drawing.Size(1034, 576);
+            this.MinimumSize = new System.Drawing.Size(780, 475);
             this.Name = "MainForm";
             this.Text = "E.M.S.";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Closing_MainForm);
